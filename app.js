1f82385ee112e678fb8e7ecdf4bd881f1a36a285
sangyoon kim <sangyun0904@gmail.com>
2022-11-05 21:11:25 +0900

    Modify app.js and install mongoose

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const app = express();
 
+//Middlewares
+app.use('/posts', () =>{
+    console.log('This is a middleware running');
+});
+
 //ROUTES
 app.get('/', (req, res) => {
     res.send('We are on home');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts');
 });
 
 app.listen(3000);
