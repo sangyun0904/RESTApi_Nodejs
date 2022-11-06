36115aa08a5fe6f35dee2940b9fd2888600a68d3
sangyoon kim <sangyun0904@gmail.com>
2022-11-06 16:53:35 +0900

    Add models folder and Post.js model install body parser

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('We are on posts');
 });
 
+
+router.get('/specific', (req, res) => {
+    res.send('Specific posts');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+});
 
 module.exports = router;
