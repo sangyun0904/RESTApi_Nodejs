c6e213cce2f88ac69c4060aa799c173b0287ac75
sangyoon kim <sangyun0904@gmail.com>
2022-11-15 19:13:28 +0900

    Modify app.js import cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 //Middlewares
